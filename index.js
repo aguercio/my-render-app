0b18b8888e7f1b5f861c2988e083ce9cc2c4fd0a
aguercio <aguercio@kent.edu>
2026-01-05 13:14:29 -0500

    Updated homepage message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Render!");
+  res.send("Hello! This is Dr. Guercio’s Web Programming II app – Version 2 🚀");
 });
 
 app.listen(PORT, () => {
